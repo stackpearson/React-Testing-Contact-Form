9427ddc2cc4739d5eb1f0fb66091778551a8e953
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-07-16 21:06:21 -0600

    mvp achieved

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,12 @@ import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 import ContactForm from './components/ContactForm';
 
-test("renders App without crashing", () => {
+test("App renders", () => {
   render(<App />);
 });
 
 //contact form renders
-test('renders contact form without crashing', () => {
+test('Conatct Form renders', () => {
   render(<ContactForm />)
 })
 
@@ -18,9 +18,14 @@ const setup = () => {
   const utils = render(<ContactForm />)
   const firstNameInput = utils.getByLabelText('first-name')
   const lastNameInput = utils.getByLabelText('last-name')
+  const emailAddy = utils.getByLabelText('email-addy')
+  const messageInput = utils.getByLabelText('message')
+
   return {
     firstNameInput,
     lastNameInput,
+    emailAddy,
+    messageInput,
     ...utils,
   }
 }
@@ -32,7 +37,7 @@ test('1st name field accepts input', () => {
 })
 
 //test that last name field accepts a name
-test('1st name field accepts input', () => {
+test('Last name field accepts input', () => {
   const { lastNameInput } = setup()
   fireEvent.change(lastNameInput, { target: { value: 'Pearson' } })
   expect(lastNameInput.value).toBe('Pearson')
@@ -40,11 +45,19 @@ test('1st name field accepts input', () => {
 
 
 //test that email field accepts an email
-
+test('email field accepts input', () => {
+  const { emailAddy } = setup()
+  fireEvent.change(emailAddy, { target: { value: 'sawyer.pearson92@gmail.com' } })
+  expect(emailAddy.value).toBe('sawyer.pearson92@gmail.com')
+})
 
 
 //test that message field accepts a message
+test('message field accepts input', () => {
+  const { messageInput } = setup()
+  fireEvent.change(messageInput, { target: { value: 'some text goes here' } })
+  expect(messageInput.value).toBe('some text goes here')
+})
 
 
 
-//test that submit button functions & reference that all fields match json output
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -19,10 +19,10 @@ const ContactForm = () => {
             name="firstName"
             placeholder="Edd"
             aria-label='first-name'
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true, minLength: 2 })}
           />
           {errors.firstName && (
-            <p>Looks like there was an error: {errors.firstName.type}</p>
+            <p aria-label='first-name-error'>Looks like there was an error: {errors.firstName.type}</p>
           )}
         </div>
 
@@ -31,10 +31,11 @@ const ContactForm = () => {
           <input
             name="lastName"
             placeholder="Burke"
+            aria-label='last-name'
             ref={register({ required: true })}
           />
           {errors.lastName && (
-            <p>Looks like there was an error: {errors.lastName.type}</p>
+            <p aria-label='last-name-error'>Looks like there was an error: {errors.lastName.type}</p>
           )}
         </div>
 
@@ -42,21 +43,21 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" aria-label='email-addy' ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea name="message" aria-label='message' ref={register({ required: false })} />
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre aria-label='submission-data' style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input aria-label='submit-button' type="submit" />
       </form>
     </div>
   );
